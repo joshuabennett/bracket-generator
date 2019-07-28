382dd4c8d2c31d32f9c9d1e640510026e79c4562
Joshua Bennett <joshuabennett@utexas.edu>
2019-07-28 00:44:08 -0500

    Added matchups for Round Robin and additional styling.

--- src/components/RoundRobin.vue
+++ src/components/RoundRobin.vue
@@ -8,7 +8,7 @@
                 <p class="panel-tabs">
                     <a v-for='n in Number(bracketInfo.groups)' @click='loadGroup(n)'>Group {{n}}</a>
                 </p>
-                <div class="panel-block" v-for='n in curGroup'>
+                <div class="panel-block" v-for='n in curGroup' v-if='!submitted'>
                     <p class="control has-icons-left">
                         <input class="input is-small" type="text" placeholder="Player Name" v-model='n.name'>
                         <span class="icon is-small is-left">
@@ -16,10 +16,25 @@
                         </span>
                     </p>
                 </div>
+                <a class="panel-block" v-for='(n, index) in curGroup' v-if='submitted' :class="{'top2' : index == 0 || index == 1}">
+                    <span class="panel-icon">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <span class="tag is-info">
+                        {{n.wins}} - {{n.losses}}
+                    </span>
+                    {{n.name}}
+                </a>
             </div>
-            <button class="button" v-for='n in curGroup'>{{n.name}} </button>
+            <div class="matchup-container">
+                <h1 class="title is-size-5 is-marginless">Matchups</h1>
+                <div class="pairing" v-for='matchup in curMatchups' v-if='submitted'> 
+                    <button class="button is-info">{{matchup.player1.name}}</button>
+                    <button class="button is-info">{{matchup.player2.name}}</button>
+                </div>
+            </div>
         </div>
-        <button class="button is-info submit-button">Submit Players</button>
+        <button class="button is-info submit-button" @click='submitted = true'>Submit Players</button>
     </div>
 </template>
 
@@ -28,7 +43,8 @@ export default {
     data: function() {
         return {
             groups: [],
-            activeGroup: 0
+            activeGroup: 0,
+            submitted: false
         }
     },
     props: ['bracketInfo'],
@@ -40,6 +56,20 @@ export default {
     computed: {
         curGroup() {
             return this.groups[this.activeGroup];
+        },
+        curMatchups() {
+            var matchups = [];
+            for (let i = 0; i < this.curGroup.length; i++) {
+                for (let j = i + 1; j < this.curGroup.length; j++) {
+                    matchups.push({
+                        player1: this.curGroup[i],
+                        player2: this.curGroup[j],
+                        winner: '',
+                        loser: ''
+                    });
+                }
+            }
+        return matchups;
         }
     },
     created() {
@@ -60,7 +90,7 @@ export default {
 
 <style>
 .panel {
-    padding: 25px;
+    min-width: 300px;
 }
 .wrapper {
     flex-direction: column;
@@ -68,5 +98,21 @@ export default {
 .container1 {
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    padding: 25px;
+}
+.matchup-container {
+    display: flex;
+    flex-direction: column;
+}
+.pairing button {
+    min-width: 150px;
+    margin: 2px;
+}
+.tag {
+    margin-right: 10px;
+}
+.top2 {
+    background: rgba(6, 245, 6, 0.432);
 }
 </style>
